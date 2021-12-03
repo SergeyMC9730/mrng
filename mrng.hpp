4cd6e964bfc3a629553b1ceaf07b144049cd3552
SergeyMC9730 <bdsservers@yandex.ru>
2021-12-03 14:05:44 +0900

    Update mrng.hpp

--- mrng.hpp
+++ mrng.hpp
@@ -4,7 +4,7 @@
 
 inline int mrng(){
   uintptr_t base = 0;
-  int rng = -(static_cast<long int>(reinterpret_cast<uintptr_t>(&base) / 256);
+  int rng = -(static_cast<long int>(reinterpret_cast<uintptr_t>(&base) / 256));
   
   return rng;
 }
